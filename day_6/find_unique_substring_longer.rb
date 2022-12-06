922ccdbed6e38987cbbb3ad6b3bc2bc0c711adb6
Felipe <suerofrancisco@yahoo.com>
2022-12-06 04:01:56 -0500

    Day 6

---
+++ day_6/find_unique_substring_longer.rb
@@ -0,0 +1,20 @@
+def read_file
+    data = File.read("input.txt")
+end
+
+def find_unique_substring(data)
+    # we're going to scan this string keeping count of how many characters we've seen
+    # whenever we have a string of four unique characters, we'll return how many characters we've seen
+
+    char_arr = data.split("")
+    substring = []
+    chars_seen = 0
+    char_arr.each do |char|
+        chars_seen += 1
+        substring << char
+        substring.shift if substring.length > 14
+        return chars_seen if  substring.uniq.length == 14
+    end
+end
+
+p find_unique_substring(read_file)
